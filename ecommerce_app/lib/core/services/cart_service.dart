97c157b0a30227dc91c23356afc11a6f3adbb56d
aadarsh0507 <infor0507@gmail.com>
2025-03-27 23:01:23 +0530

    Add ecommerce_app/lib/core/services/cart_service.dart 1743096664287

---
+++ ecommerce_app/lib/core/services/cart_service.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/foundation.dart';
+import 'package:ecommerce_app/core/models/product.dart';
+
+class CartItem {
+  final Product product;
+  int quantity;
+
+  CartItem({
+    required this.product,
+    this.quantity = 1,
+  });
+
+  double get totalPrice => product.price * quantity;
+}
+
+class CartService with ChangeNotifier {
+  final List<CartItem> _items = [];
+
+  List<CartItem> get items => _items;
+
+  int get itemCount => _items.fold(0, (sum, item) => sum + item.quantity);
+
+  double get totalPrice =>
+      _items.fold(0, (sum, item) => sum + item.totalPrice);
+
+  void addToCart(Product product, [int quantity = 1]) {
+    final index = _items.indexWhere((item) => item.product.id == product.id);
+    if (index >= 0) {
+      _items[index].quantity += quantity;
+    } else {
+      _items.add(CartItem(product: product, quantity: quantity));
+    }
+    notifyListeners();
+  }
+
+  void removeFromCart(String productId) {
+    _items.removeWhere((item) => item.product.id == productId);
+    notifyListeners();
+  }
+
+  void updateQuantity(String productId, int newQuantity) {
+    final index = _items.indexWhere((item) => item.product.id == productId);
+    if (index >= 0) {
+      _items[index].quantity = newQuantity;
+      notifyListeners();
+    }
+  }
+
+  void clearCart() {
+    _items.clear();
+    notifyListeners();
+  }
+
+  bool isInCart(String productId) {
+    return _items.any((item) => item.product.id == productId);
+  }
+}
